63f2148b402013b06919d12399c2cb649e1d21ea
Helmut Kemper <helmut.kemper@gmail.com>
2019-12-04 10:52:25 -0300

    trabalhando

--- abstractType/basicBox/newInk.go
+++ abstractType/basicBox/newInk.go
@@ -5,14 +5,11 @@ import (
 	iotmaker_platform_coordinate "github.com/helmutkemper/iotmaker.platform.coordinate"
 )
 
-func NewInc(lineWidth int, shadow iotmaker_platform_IDraw.IFilterShadowInterface, gradient iotmaker_platform_IDraw.IFilterGradientInterface, density interface{}, iDensity iotmaker_platform_coordinate.IDensity) Ink {
+func NewInc(ink Ink, lineWidth int, shadow iotmaker_platform_IDraw.IFilterShadowInterface, gradient iotmaker_platform_IDraw.IFilterGradientInterface, density interface{}, iDensity iotmaker_platform_coordinate.IDensity) Ink {
 	densityCalc := iDensity
 	densityCalc.Set(lineWidth)
 	densityCalc.SetDensityFactor(density)
 
-	return Ink{
-		LineWidth: densityCalc.Int(),
-		Shadow:    shadow,
-		Gradient:  gradient,
-	}
+	ink.Set(densityCalc.Int(), shadow, gradient)
+	return ink
 }
