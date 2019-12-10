c566aae8dd0c9822709efbb70dc78580df7db0f6
Helmut Kemper <helmut.kemper@gmail.com>
2019-12-10 13:22:14 -0300

    trabalhando

--- abstractType/selectBox/newResizeBox.go
+++ abstractType/selectBox/newResizeBox.go
@@ -6,7 +6,7 @@ import (
 	"github.com/helmutkemper/iotmaker.platform/abstractType/genericTypes"
 )
 
-func NewResizeBoxFromBasicBob(basicBox *basicBox.BasicBox, offsetX, offsetY, width, height, border int, density interface{}, iDensity iotmaker_platform_coordinate.IDensity) *ResizeBoxes {
+func NewResizeBoxFromBasicBox(basicBox *basicBox.BasicBox, offsetX, offsetY, width, height, border int, density interface{}, iDensity iotmaker_platform_coordinate.IDensity) *ResizeBoxes {
 
 	dm := genericTypes.Dimensions{}
 	dm = genericTypes.NewDimensions(dm, offsetX, offsetY, width, height, border, density, iDensity)
