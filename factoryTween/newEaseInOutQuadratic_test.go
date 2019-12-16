f5aba51f30be873bb20e97d68f58ab10ebade31d
Helmut Kemper <helmut.kemper@gmail.com>
2019-12-16 16:34:31 -0300

    tween test

---
+++ factoryTween/newEaseInOutQuadratic_test.go
@@ -0,0 +1,45 @@
+package factoryTween
+
+import (
+	"fmt"
+	"github.com/helmutkemper/iotmaker.platform/fps"
+	"github.com/helmutkemper/util"
+	"sync"
+	"time"
+)
+
+func ExampleTween_NewEaseInOutQuadratic() {
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+
+	fps.Set(3)
+
+	start := time.Now()
+
+	NewEaseInOutQuadratic(
+		time.Second*2,
+		0,
+		3,
+		func(value float64) {
+			fmt.Printf("value on interaction event: %v\n", util.Round(value, 0.5, 1.0))
+		},
+		func(value float64) {
+			wg.Done()
+			fmt.Printf("value on done event: %v\n", util.Round(value, 0.5, 1.0))
+		},
+	)
+
+	wg.Wait()
+	elapsed := time.Since(start)
+	fmt.Printf("total time: %v", util.Round(elapsed.Seconds(), 0.5, 1.0))
+
+	//output:
+	//value on interaction event: 0.2
+	//value on interaction event: 0.7
+	//value on interaction event: 1.5
+	//value on interaction event: 2.3
+	//value on interaction event: 2.8
+	//value on interaction event: 3
+	//value on done event: 3
+	//total time: 2
+}
