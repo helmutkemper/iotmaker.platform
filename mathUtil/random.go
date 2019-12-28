18b3d528f036a8cf3953f5cf0d28459395f7d7ef
Helmut Kemper <helmut.kemper@gmail.com>
2019-12-28 09:43:02 -0300

    trabalhando

---
+++ mathUtil/random.go
@@ -0,0 +1,17 @@
+package mathUtil
+
+import (
+	"math/rand"
+	"time"
+)
+
+var secureRand *rand.Rand
+
+func Float64FomInt(min, max int) float64 {
+	return float64(secureRand.Int63n(int64(max-min)) + int64(min))
+}
+
+func init() {
+	s1 := rand.NewSource(time.Now().UnixNano())
+	secureRand = rand.New(s1)
+}
