cf2bcc17e95e025742b9005da8376eda7a154a88
helmutkemper <helmut.kemper@ahgora.com.br>
2019-12-30 10:17:45 -0300

    tween

---
+++ factoryTween/newEaseInQuadraticOutSine.go
@@ -0,0 +1,23 @@
+package factoryTween
+
+import (
+	"github.com/helmutkemper/iotmaker.platform/tween"
+	"time"
+)
+
+func NewEaseInQuadraticOutSine(duration time.Duration, startValue, endValue float64, interactionFunc func(value, percentToComplete float64, arguments []interface{}), onStartFunc func(value float64), onEndFunc func(value float64), arguments ...interface{}) *tween.Tween {
+	t := &tween.Tween{
+		OnStart:     onStartFunc,
+		OnEnd:       onEndFunc,
+		Arguments:   arguments,
+		Duration:    duration,
+		StartValue:  startValue,
+		EndValue:    endValue,
+		Func:        tween.KEaseInQuadraticOutSine,
+		Interaction: interactionFunc,
+		Repeat:      0,
+	}
+	t.Start()
+
+	return t
+}
---
+++ factoryTween/newEaseInQuarticOutQuadratic.go
@@ -0,0 +1,23 @@
+package factoryTween
+
+import (
+	"github.com/helmutkemper/iotmaker.platform/tween"
+	"time"
+)
+
+func NewEaseInQuarticOutQuadratic(duration time.Duration, startValue, endValue float64, interactionFunc func(value, percentToComplete float64, arguments []interface{}), onStartFunc func(value float64), onEndFunc func(value float64), arguments ...interface{}) *tween.Tween {
+	t := &tween.Tween{
+		OnStart:     onStartFunc,
+		OnEnd:       onEndFunc,
+		Arguments:   arguments,
+		Duration:    duration,
+		StartValue:  startValue,
+		EndValue:    endValue,
+		Func:        tween.KEaseInQuarticOutQuadratic,
+		Interaction: interactionFunc,
+		Repeat:      0,
+	}
+	t.Start()
+
+	return t
+}
