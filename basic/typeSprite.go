8c1d4da15db513c70c9d0483a29e55d5e9384fa3
Helmut Kemper <helmut.kemper@gmail.com>
2020-01-05 11:43:09 -0300

    trabalhando

--- basic/typeSprite.go
+++ basic/typeSprite.go
@@ -4,10 +4,38 @@ import (
 	iotmaker_platform_IDraw "github.com/helmutkemper/iotmaker.santa_isabel_theater.platform.IDraw"
 	"github.com/helmutkemper/iotmaker.santa_isabel_theater.platform/abstractType/genericTypes"
 	platformMouse "github.com/helmutkemper/iotmaker.santa_isabel_theater.platform/mouse"
+	"strings"
+)
+
+type DragMode int
+
+func (el DragMode) String() string {
+	return dragModeString[el]
+}
+
+func (el DragMode) FromString(value string) DragMode {
+	return dragModeFromStringMap[strings.ToLower(value)]
+}
+
+var dragModeFromStringMap = map[string]DragMode{
+	"desktop": KDragModeDesktop,
+	"mobile":  KDragModeMobile,
+}
+
+var dragModeString = [...]string{
+	"desktop",
+	"mobile",
+}
+
+const (
+	KDragModeDesktop DragMode = iota
+	KDragModeMobile
 )
 
 type Drag struct {
+	dragId      string
 	isDraggable bool
+	dragMode    DragMode
 }
 
 type Sprite struct {
@@ -20,6 +48,10 @@ type Sprite struct {
 	prepareShadowFilterFunctionPointer   func(iotmaker_platform_IDraw.ICanvasShadow)
 	prepareGradientFilterFunctionPointer func(iotmaker_platform_IDraw.ICanvasGradient)
 	Drag
+}
+
+func (el *Sprite) SetDragMode(mode DragMode) {
+	el.dragMode = mode
 }
 
 func (el *Sprite) SetPlatform(platform iotmaker_platform_IDraw.IDraw) {
@@ -38,18 +70,25 @@ func (el *Sprite) DragStop() {
 	el.setDraggable(false)
 }
 
+func (el *Sprite) SetDraggableToDesktop() {
+	platformMouse.AddFunctionPointer(el.Id+"DraggableImage", el.GetCollisionBox, func(x, y float64, collision bool) {
+		el.OutBoxDimensions.X = x - el.OutBoxDimensions.Width/2
+		el.Dimensions.X = x - el.Dimensions.Width/2
+		el.OutBoxDimensions.Y = y - el.OutBoxDimensions.Height/2
+		el.Dimensions.Y = y - el.Dimensions.Height/2
+	})
+}
+
+func (el *Sprite) RemoveDraggableToDesktop() {
+	platformMouse.RemoveFunctionPointer(el.Id + "DraggableImage")
+}
+
 func (el *Sprite) setDraggable(enable bool) {
-	id := el.Id + "DraggableImage"
 	el.isDraggable = enable
 	if enable == true {
-		platformMouse.AddFunctionPointer(id, el.GetCollisionBox, func(x, y float64, collision bool) {
-			el.OutBoxDimensions.X = x - el.OutBoxDimensions.Width/2
-			el.Dimensions.X = x - el.Dimensions.Width/2
-			el.OutBoxDimensions.Y = y - el.OutBoxDimensions.Height/2
-			el.Dimensions.Y = y - el.Dimensions.Height/2
-		})
+		el.SetDraggableToDesktop()
 	} else {
-		platformMouse.RemoveFunctionPointer(id)
+		el.RemoveDraggableToDesktop()
 	}
 }
 
