47aed1be084ba9d247a2e9c5650376db12e5aded
Helmut Kemper <helmut.kemper@gmail.com>
2020-01-05 13:53:19 -0300

    trabalhando

--- mouse/AddFunctionPointer.go
+++ mouse/AddFunctionPointer.go
@@ -30,6 +30,125 @@ func ManagerMouseMove(x, y float64) {
 	}
 }
 
+func ManagerMouseDown(x, y float64) {
+	var isOver bool
+
+	for id, listFuncTestOver := range listTestEventOverFunctions {
+
+		for k, funcTestOver := range listFuncTestOver {
+
+			if funcTestOver != nil {
+
+				isOver = funcTestOver(x, y)
+				listOnEventFunctions[id][k](x, y, isOver)
+
+			}
+		}
+	}
+}
+
+func ManagerMouseUp(x, y float64) {
+	var isOver bool
+
+	for id, listFuncTestOver := range listTestEventOverFunctions {
+
+		for k, funcTestOver := range listFuncTestOver {
+
+			if funcTestOver != nil {
+
+				isOver = funcTestOver(x, y)
+				listOnEventFunctions[id][k](x, y, isOver)
+
+			}
+		}
+	}
+}
+
+func ManagerClick(x, y float64) {
+	var isOver bool
+
+	for id, listFuncTestOver := range listTestEventOverFunctions {
+
+		for k, funcTestOver := range listFuncTestOver {
+
+			if funcTestOver != nil {
+
+				isOver = funcTestOver(x, y)
+				listOnEventFunctions[id][k](x, y, isOver)
+
+			}
+		}
+	}
+}
+
+func ManagerDoubleClick(x, y float64) {
+	var isOver bool
+
+	for id, listFuncTestOver := range listTestEventOverFunctions {
+
+		for k, funcTestOver := range listFuncTestOver {
+
+			if funcTestOver != nil {
+
+				isOver = funcTestOver(x, y)
+				listOnEventFunctions[id][k](x, y, isOver)
+
+			}
+		}
+	}
+}
+
+func ManagerContextMenu(x, y float64) {
+	var isOver bool
+
+	for id, listFuncTestOver := range listTestEventOverFunctions {
+
+		for k, funcTestOver := range listFuncTestOver {
+
+			if funcTestOver != nil {
+
+				isOver = funcTestOver(x, y)
+				listOnEventFunctions[id][k](x, y, isOver)
+
+			}
+		}
+	}
+}
+
+func ManagerMouseEnter(x, y float64) {
+	var isOver bool
+
+	for id, listFuncTestOver := range listTestEventOverFunctions {
+
+		for k, funcTestOver := range listFuncTestOver {
+
+			if funcTestOver != nil {
+
+				isOver = funcTestOver(x, y)
+				listOnEventFunctions[id][k](x, y, isOver)
+
+			}
+		}
+	}
+}
+
+func ManagerMouseOut(x, y float64) {
+	var isOver bool
+
+	for id, listFuncTestOver := range listTestEventOverFunctions {
+
+		for k, funcTestOver := range listFuncTestOver {
+
+			if funcTestOver != nil {
+
+				isOver = funcTestOver(x, y)
+				listOnEventFunctions[id][k](x, y, isOver)
+
+			}
+		}
+	}
+}
+
 func AddFunctionPointer(id string, collisionFunction PointerCollisionFunction, positiveEventFunction PointerEventFunction) {
 	if len(listTestEventOverFunctions) == 0 {
 		listTestEventOverFunctions = make(map[string][]PointerCollisionFunction)
---
+++ mouse/typeEvent.go
@@ -0,0 +1,74 @@
+package mouse
+
+import "strings"
+
+type EventMouse int
+
+func (el EventMouse) String() string {
+	return eventMouseString[el]
+}
+
+func (el EventMouse) StringToType(value string) EventMouse {
+	return eventFromStringMap[strings.ToLower(value)]
+}
+
+var eventFromStringMap = map[string]EventMouse{
+	"click":       KClick,
+	"contextMenu": KContextMenu,
+	"doubleClick": KDoubleClick,
+	"mouseDown":   KMouseDown,
+	"mouseEnter":  KMouseEnter,
+	"mouseLeave":  KMouseLeave,
+	"mouseMove":   KMouseMove,
+	"mouseOver":   KMouseOver,
+	"mouseOut":    KMouseOut,
+	"mouseUp":     KMouseUp,
+}
+
+var eventMouseString = [...]string{
+	"click",
+	"contextMenu",
+	"doubleClick",
+	"mouseDown",
+	"mouseEnter",
+	"mouseLeave",
+	"mouseMove",
+	"mouseOver",
+	"mouseOut",
+	"mouseUp",
+}
+
+const (
+	// en: The event occurs when the user clicks on an element
+	KClick EventMouse = iota
+
+	// en: The event occurs when the user right-clicks on an element to open a context
+	// menu
+	KContextMenu
+
+	// en: The event occurs when the user double-clicks on an element
+	KDoubleClick
+
+	// en: The event occurs when the user presses a mouse button over an element
+	KMouseDown
+
+	// en: The event occurs when the pointer is moved onto an element
+	KMouseEnter
+
+	// en: The event occurs when the pointer is moved out of an element
+	KMouseLeave
+
+	// en: The event occurs when the pointer is moving while it is over an element
+	KMouseMove
+
+	// en: The event occurs when the pointer is moved onto an element, or onto one of
+	// its children
+	KMouseOver
+
+	// en: The event occurs when a user moves the mouse pointer out of an element, or
+	// out of one of its children
+	KMouseOut
+
+	// en: The event occurs when a user releases a mouse button over an element
+	KMouseUp
+)
