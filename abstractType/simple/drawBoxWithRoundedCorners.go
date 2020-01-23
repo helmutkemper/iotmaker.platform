221c31eeb5d24b1202076471f8bbd0f57511a9b8
Helmut Kemper <helmut.kemper@gmail.com>
2020-01-22 21:11:11 -0300

    trabalhando

--- abstractType/simple/drawBoxWithRoundedCorners.go
+++ abstractType/simple/drawBoxWithRoundedCorners.go
@@ -1,7 +1,7 @@
 package simple
 
 import (
-	iotmaker_platform_IDraw "github.com/helmutkemper/iotmaker.santa_isabel_theater.platform.IDraw"
+	"github.com/helmutkemper/iotmaker.santa_isabel_theater.platform/basic"
 )
 
 // en: Draw a rectangle box with a rounded corners [optional]
@@ -11,55 +11,41 @@ import (
 // pt_br: Desenha um retângulo com bordas arredondadas [opcional]
 //     Use a borda igual a metade da altura para um fazer as pontas do desenho serem
 //     um semicírculo
-//
-//              border        border
-//             x1  x2         x3 x4
-//           l     a          b     c
-//        y1    +--|----------|--+    y1
-//  border      |                |      border
-//        y2 k ---              --- d y2
-//              |                |
-//              |                |
-//        y3 j ---              --- e y3
-//  border      |                |      border
-//        y4    +--|----------|--+    y4
-//           i     h          g     f
-//             x1  x2         x3 x4
-//              border        border
-//
 type BoxWithRoundedCorners struct {
-	Platform iotmaker_platform_IDraw.IDraw
+	basic.Sprite
 
-	X      int
-	Y      int
-	Width  int
-	Height int
-	Border int
+	x1 float64
+	x2 float64
+	x3 float64
+	x4 float64
 
-	x1 int
-	x2 int
-	x3 int
-	x4 int
-
-	y1 int
-	y2 int
-	y3 int
-	y4 int
+	y1 float64
+	y2 float64
+	y3 float64
+	y4 float64
 }
 
 // en: Calculate a rectangle
 //
 // pt_br: Calcula o retângulo
 func (el *BoxWithRoundedCorners) Init() {
-	el.x1 = el.X
-	el.x2 = el.x1 + el.Border
-	el.x3 = el.x2 + el.Width - 2*el.Border
-	el.x4 = el.x3 + el.Border
+	el.x1 = el.Dimensions.X
+	el.x2 = el.x1 + el.Dimensions.Border
+	el.x3 = el.x2 + el.Dimensions.Width - 2*el.Dimensions.Border
+	el.x4 = el.x3 + el.Dimensions.Border
 
-	el.y1 = el.Y
-	el.y2 = el.y1 + el.Border
-	el.y3 = el.y2 + el.Height - 2*el.Border
-	el.y4 = el.y3 + el.Border
+	el.y1 = el.Dimensions.Y
+	el.y2 = el.y1 + el.Dimensions.Border
+	el.y3 = el.y2 + el.Dimensions.Height - 2*el.Dimensions.Border
+	el.y4 = el.y3 + el.Dimensions.Border
+}
+
+func (el *BoxWithRoundedCorners) Crete() {
+	el.Draw()
+}
+
+func (el *BoxWithRoundedCorners) Clear() {
+	el.ScratchPad.ClearRect(el.Dimensions.X, el.Dimensions.Y, el.Dimensions.Width, el.Dimensions.Height)
 }
 
 // en: Draw function
@@ -82,19 +68,17 @@ func (el *BoxWithRoundedCorners) Draw() {
 	//             x1  x2         x3 x4
 	//              border        border
 
-	if el.Platform == nil {
-		return
-	}
+	el.ScratchPad.BeginPath()
+	el.ScratchPad.MoveTo(el.x2, el.y1)                                    // a
+	el.ScratchPad.LineTo(el.x3, el.y1)                                    // a->b
+	el.ScratchPad.ArcTo(el.x4, el.y1, el.x4, el.y2, el.Dimensions.Border) // c->d
+	el.ScratchPad.LineTo(el.x4, el.y3)                                    // d->e
+	el.ScratchPad.ArcTo(el.x4, el.y4, el.x3, el.y4, el.Dimensions.Border) // f->g
+	el.ScratchPad.LineTo(el.x2, el.y4)                                    // g->h
+	el.ScratchPad.ArcTo(el.x1, el.y4, el.x1, el.y3, el.Dimensions.Border) // i->j
+	el.ScratchPad.LineTo(el.x1, el.y2)                                    // j->k
+	el.ScratchPad.ArcTo(el.x1, el.y1, el.x2, el.y1, el.Dimensions.Border) // i->j
+	el.ScratchPad.ClosePath(el.x2, el.y1)                                 // a
 
-	el.Platform.BeginPath()
-	el.Platform.MoveTo(el.x2, el.y1)                         // a
-	el.Platform.LineTo(el.x3, el.y1)                         // a->b
-	el.Platform.ArcTo(el.x4, el.y1, el.x4, el.y2, el.Border) // c->d
-	el.Platform.LineTo(el.x4, el.y3)                         // d->e
-	el.Platform.ArcTo(el.x4, el.y4, el.x3, el.y4, el.Border) // f->g
-	el.Platform.LineTo(el.x2, el.y4)                         // g->h
-	el.Platform.ArcTo(el.x1, el.y4, el.x1, el.y3, el.Border) // i->j
-	el.Platform.LineTo(el.x1, el.y2)                         // j->k
-	el.Platform.ArcTo(el.x1, el.y1, el.x2, el.y1, el.Border) // i->j
-	el.Platform.ClosePath(el.x2, el.y1)                      // a
+	el.ScratchPad.Stroke()
 }
--- factorySimpleBox/newBoxWithRoundedCorners.go
+++ factorySimpleBox/newBoxWithRoundedCorners.go
@@ -3,13 +3,16 @@ package factorySimpleBox
 import (
 	iotmaker_platform_IDraw "github.com/helmutkemper/iotmaker.santa_isabel_theater.platform.IDraw"
 	iotmaker_platform_coordinate "github.com/helmutkemper/iotmaker.santa_isabel_theater.platform.coordinate"
+	"github.com/helmutkemper/iotmaker.santa_isabel_theater.platform/abstractType/genericTypes"
 	"github.com/helmutkemper/iotmaker.santa_isabel_theater.platform/abstractType/simple"
+	"github.com/helmutkemper/iotmaker.santa_isabel_theater.platform/basic"
+	stageEngine "github.com/helmutkemper/iotmaker.santa_isabel_theater.platform/engine"
 )
 
 // en: This function create a path of a box with rounded corners into the platform using density
 //
 // pt_br: Esta função cria o caminho de um quadrado com cantos arredondados na plataforma usando densidade
-func NewBoxWithRoundedCorners(platform iotmaker_platform_IDraw.IDraw, x, y, width, height, border int, density interface{}, iDensity iotmaker_platform_coordinate.IDensity) simple.BoxWithRoundedCorners {
+func NewBoxWithRoundedCorners(id string, engine stageEngine.IEngine, platform, scratchPad iotmaker_platform_IDraw.IDraw, x, y, width, height, border int, density interface{}, iDensity iotmaker_platform_coordinate.IDensity) *simple.BoxWithRoundedCorners {
 	densityCalc := iDensity
 	densityCalc.SetDensityFactor(density)
 
@@ -28,15 +31,29 @@ func NewBoxWithRoundedCorners(platform iotmaker_platform_IDraw.IDraw, x, y, widt
 	densityCalc.SetInt(border)
 	border = densityCalc.Int()
 
-	rect := simple.BoxWithRoundedCorners{
-		X:      x,
-		Y:      y,
-		Width:  width,
-		Height: height,
-		Border: border,
+	rect := &simple.BoxWithRoundedCorners{
+		Sprite: basic.Sprite{
+			Id:         id,
+			Engine:     engine,
+			Platform:   platform,
+			ScratchPad: scratchPad,
+			Dimensions: genericTypes.Dimensions{
+				X:      float64(x),
+				Y:      float64(y),
+				Width:  float64(width),
+				Height: float64(height),
+				Border: float64(border),
+			},
+			OutBoxDimensions: genericTypes.Dimensions{
+				X:      float64(x),
+				Y:      float64(y),
+				Width:  float64(width),
+				Height: float64(height),
+			},
+		},
 	}
 	rect.Init()
-	rect.Draw()
+	rect.Crete()
 
 	return rect
 }
