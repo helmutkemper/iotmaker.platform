53f721dc2d900d4054433be7794ea1662560d398
Helmut Kemper <helmut.kemper@gmail.com>
2020-01-28 20:39:47 -0300

    trabalhando

---
+++ factoryColorNames/newSlateblueHalfTransparent.go
@@ -0,0 +1,7 @@
+package factoryColorNames
+
+import "image/color"
+
+func NewSlateblueHalfTransparent() color.RGBA {
+	return color.RGBA{R: 0x6a, G: 0x5a, B: 0xcd, A: 0x80} // rgb(106, 90, 205)
+}
---
+++ factoryColorNames/newSteelblueHalfTransparent.go
@@ -0,0 +1,7 @@
+package factoryColorNames
+
+import "image/color"
+
+func NewSteelblueHalfTransparent() color.RGBA {
+	return color.RGBA{R: 0x46, G: 0x82, B: 0xb4, A: 0x80} // rgb(70, 130, 180)
+}
