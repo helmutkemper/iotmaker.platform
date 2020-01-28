f5ff69fa69c26883d42c7a4484c59ed23a9aed62
Helmut Kemper <helmut.kemper@gmail.com>
2020-01-28 20:58:06 -0300

    trabalhando

---
+++ factoryColorNames/newMediumaquamarineHalfTransparent.go
@@ -0,0 +1,7 @@
+package factoryColorNames
+
+import "image/color"
+
+func NewMediumaquamarineHalfTransparent() color.RGBA {
+	return color.RGBA{R: 0x66, G: 0xcd, B: 0xaa, A: 0x80} // rgb(102, 205, 170)
+}
