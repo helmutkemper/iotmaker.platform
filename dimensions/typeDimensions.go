eaec35e7a7c355c1a9072823449af4fc272071af
Helmut Kemper <helmut.kemper@gamil.com>
2020-02-08 09:23:24 -0300

    trabalhando

--- dimensions/typeDimensions.go
+++ dimensions/typeDimensions.go
@@ -22,9 +22,15 @@ type Link struct {
 	CornerFromB Corner
 }
 
+// en: It calculates the vertical position of the container when there is only the
+// link on the bottom are connected to
+//
+// pt_br: Calcula o posicionamento vertical do container quando só existe o link
+// inferior conectado
+//
 //                A
 //     +----------O---------+
-//     |          X         |
+//     |                    |
 //     |   +------O-----+   |
 //   D O   O            O   O B
 //     |   +------O-----+   |
@@ -39,13 +45,19 @@ func (el *Link) LinkIsPresentAtTheBottom(containerA, containerB *Dimensions) {
 	containerB.Y = AY + AHeight - BHeight + BBottom
 }
 
+// en: It calculates the vertical position of the container when there is only the
+// link on the top are connected to
+//
+// pt_br: Calcula o posicionamento vertical do container quando só existe o link
+// superior conectado
+//
 //                A
 //     +----------O---------+
 //     |          |         |
 //     |   +------O-----+   |
 //   D O   O            O   O B
 //     |   +------O-----+   |
-//     |          X         |
+//     |                    |
 //     +----------O---------+
 //                C
 func (el *Link) LinkIsPresentAtTheTop(containerA, containerB *Dimensions) {
@@ -54,6 +66,12 @@ func (el *Link) LinkIsPresentAtTheTop(containerA, containerB *Dimensions) {
 	containerB.Y = AY + BTop
 }
 
+// en: Calculates the vertical position of the container when the links to the top
+// and bottom are connected
+//
+// pt_br: Calcula o posicionamento vertical do container quando existe os links
+// superior e inferior conectados
+//
 //                A
 //     +----------O---------+
 //     |          |         |
@@ -77,11 +95,17 @@ func (el *Link) LinkIsPresentAtTheTopAndBottom(containerA, containerB *Dimension
 	containerB.Y = BY
 }
 
+// en: It calculates the horizontal position of the container when there is only the
+// link on the right are connected to
+//
+// pt_br: Calcula o posicionamento horizontal do container quando só existe o link
+// a direita conectado
+//
 //                A
 //     +----------O---------+
 //     |                    |
 //     |   +------O-----+   |
-//   D O-X-O            O---O B
+//   D O   O            O---O B
 //     |   +------O-----+   |
 //     |                    |
 //     +----------O---------+
@@ -93,11 +117,17 @@ func (el *Link) LinkIsPresentAtTheRight(containerA, containerB *Dimensions) {
 	containerB.X = AX + AWidth - CWidth
 }
 
+// en: It calculates the horizontal position of the container when there is only the
+// link on the left are connected to
+//
+// pt_br: Calcula o posicionamento horizontal do container quando só existe o link
+// a esquerda conectado
+//
 //                A
 //     +----------O---------+
 //     |                    |
 //     |   +------O-----+   |
-//   D O---O            O-X-O B
+//   D O---O            O   O B
 //     |   +------O-----+   |
 //     |                    |
 //     +----------O---------+
@@ -107,6 +137,12 @@ func (el *Link) LinkIsPresentAtTheLeft(containerA, containerB *Dimensions) {
 	containerB.X = AX
 }
 
+// en: Calculates the horizontal position of the container when the links to the
+// right and the left are connected
+//
+// pt_br: Calcula o posicionamento horizontal do container quando existe os links
+// direito e esquerdos conectados
+//
 //                A
 //     +----------O---------+
 //     |                    |
@@ -123,7 +159,7 @@ func (el *Link) LinkIsPresentAtTheRightAndLeft(containerA, containerB *Dimension
 	containerB.X = AX + AWidth/2 - CWidth/2
 }
 
-func (el *Link) SimpleLinkAssembly(linkA, linkB, linkC, linkD *Link) {
+func (el *Link) SimpleLinkAssembly(linkA, linkB, linkC, linkD *Link) error {
 
 	//                      A
 	//             +--------O-------+
@@ -134,15 +170,13 @@ func (el *Link) SimpleLinkAssembly(linkA, linkB, linkC, linkD *Link) {
 	//             |        |cPass  |
 	//             +--------O-------+
 	//                      C
-	err := make([]error, 0)
-
 	aPass := !reflect.DeepEqual(*linkA, Link{})
 	bPass := !reflect.DeepEqual(*linkB, Link{})
 	cPass := !reflect.DeepEqual(*linkC, Link{})
 	dPass := !reflect.DeepEqual(*linkD, Link{})
 
 	if aPass == false && cPass == false {
-		err = append(err, errors.New("this container can't be assembled. links a and c are not connected"))
+		return errors.New("this container can't be assembled. links a and c are not connected")
 	} else if aPass == false && cPass == true {
 		linkA.LinkIsPresentAtTheBottom(linkC.ContainerA, linkC.ContainerB)
 	} else if aPass == true && cPass == false {
@@ -152,7 +186,7 @@ func (el *Link) SimpleLinkAssembly(linkA, linkB, linkC, linkD *Link) {
 	}
 
 	if dPass == false && bPass == false {
-		err = append(err, errors.New("this container can't be assembled. links b and d are not connected"))
+		return errors.New("this container can't be assembled. links b and d are not connected")
 	} else if dPass == false && bPass == true {
 		linkA.LinkIsPresentAtTheRight(linkB.ContainerA, linkB.ContainerB)
 	} else if dPass == true && bPass == false {
@@ -160,6 +194,8 @@ func (el *Link) SimpleLinkAssembly(linkA, linkB, linkC, linkD *Link) {
 	} else if dPass == true && bPass == true {
 		linkA.LinkIsPresentAtTheRightAndLeft(linkD.ContainerA, linkD.ContainerB)
 	}
+
+	return nil
 }
 
 //                A
@@ -211,13 +247,50 @@ type Dimensions struct {
 
 func NewContainer(width, height int) *Dimensions {
 	return &Dimensions{
+		X:      0,
+		Y:      0,
 		Width:  width,
 		Height: height,
+
+		SpaceLeft:   0,
+		SpaceRight:  0,
+		SpaceTop:    0,
+		SpaceBottom: 0,
+	}
+}
+
+func NewContainerWidthXY(x, y, width, height int) *Dimensions {
+	return &Dimensions{
+		X:      x,
+		Y:      y,
+		Width:  width,
+		Height: height,
+
+		SpaceLeft:   0,
+		SpaceRight:  0,
+		SpaceTop:    0,
+		SpaceBottom: 0,
+	}
+}
+
+func NewContainerWidthXYAndSpaces(x, y, width, height, left, right, top, bottom int) *Dimensions {
+	return &Dimensions{
+		X:      x,
+		Y:      y,
+		Width:  width,
+		Height: height,
+
+		SpaceLeft:   left,
+		SpaceRight:  right,
+		SpaceTop:    top,
+		SpaceBottom: bottom,
 	}
 }
 
 func NewContainerWithSpace(width, height, left, right, top, bottom int) *Dimensions {
 	return &Dimensions{
+		X:      0,
+		Y:      0,
 		Width:  width,
 		Height: height,
 
@@ -265,7 +338,7 @@ func NewLink(containerA, containerB *Dimensions, cornerFromA, cornerFromB Corner
 	return errors.New("corners a and c must be linked to corners a and c, and corners b and d must be linked to corners b and d"), &Link{}
 }
 
-func Test() (Dimensions, Dimensions) {
+func Test() []Dimensions {
 	var err error
 
 	var linkFatherAtoContainerA = &Link{}
@@ -274,7 +347,8 @@ func Test() (Dimensions, Dimensions) {
 	var linkFatherDtoContainerD = &Link{}
 
 	father := NewContainer(600, 300)
-	containerA := NewContainer(300, 100)
+	containerA := NewContainerWidthXY(10, 10, 300, 100)
+	containerB := NewContainerWidthXY(10, 120, 300, 100)
 	err, linkFatherAtoContainerA = NewLink(father, containerA, KCornerA, KCornerA)
 	if err != nil {
 		fmt.Printf("error: %v\n", err)
@@ -292,12 +366,15 @@ func Test() (Dimensions, Dimensions) {
 		fmt.Printf("error: %v\n", err)
 	}
 
-	linkFatherAtoContainerA.SimpleLinkAssembly(
+	err = linkFatherAtoContainerA.SimpleLinkAssembly(
 		linkFatherAtoContainerA,
 		linkFatherBtoContainerB,
 		linkFatherCtoContainerC,
 		linkFatherDtoContainerD,
 	)
+	if err != nil {
+		fmt.Printf("SimpleLinkAssembly().error: %v\n", err.Error())
+	}
 
-	return *father, *containerA
+	return []Dimensions{*father, *containerA, *containerB}
 }
