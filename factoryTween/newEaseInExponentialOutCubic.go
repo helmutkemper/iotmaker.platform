a11c4f0e7d0877990bb187b8d8444a94936d5e54
Helmut Kemper <helmut.kemper@gmail.com>
2021-06-26 12:49:50 -0300

    refactoring

--- factoryTween/newEaseInExponentialOutCubic.go
+++ factoryTween/newEaseInExponentialOutCubic.go
@@ -12,6 +12,7 @@ import (
 //     duration: animation duration
 //     startValue: initial value
 //     endValue: final value
+//     doNotReverseMotion: não inverter o movimento para loops
 //     onStartFunc: on start function
 //     onEndFunc: on end function
 //     onCycleStartFunc: on tween cycle start function
